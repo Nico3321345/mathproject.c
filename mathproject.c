eedf957f97dfb1b938b9cb9af7d8be6c073186f9
Nico3321345 <47857830+Nico3321345@users.noreply.github.com>
2019-04-30 09:29:32 -0400

    Create mathproject.c

---
+++ mathproject.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int v, i, r;
+int x;
+float vnum[50000], inum[50000], rnum[50000];
+int main(void){
+    printf("what is v?");
+    scanf("%f",&vnum[v]);
+    printf("what is i?");
+    scanf("%f",&inum[i]);
+    printf("what is r?");
+    scanf("%f",&rnum[r]);
+    
+    if(v==x){
+        printf("Unknown");
+        if()
+   
+    }
+}
